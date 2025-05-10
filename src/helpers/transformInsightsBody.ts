284ae597bcb67d2e2a4f5aef0b3e29cb80eb9d62
Mena Mahany <mahanymena@gmail.com>
2025-05-10 15:21:33 +0300

    filling all required fields

--- src/helpers/transformInsightsBody.ts
+++ src/helpers/transformInsightsBody.ts
@@ -3,7 +3,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -13,7 +13,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -23,7 +23,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -33,7 +33,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -43,7 +43,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -53,7 +53,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -63,7 +63,7 @@ export interface InsightResponse {
     evidence: string;
     recommendation: {
       root_cause: string;
-      action_steps: [string];
+      action_steps: string[];
       business_impact: string;
       success_metrics: string;
       timeline: string;
@@ -71,14 +71,36 @@ export interface InsightResponse {
   };
 }
 
+const mainKeys = [
+  "customer_satisfaction",
+  "customer_complaints",
+  "employee_sentiment",
+  "maintenance_or_equipment_issues",
+  "product_feedback",
+  "operational_red_flags",
+  "improvement_suggestions",
+];
+
 export const transformInsightsBody = (llmInsights: InsightResponse) => {
   const result: Partial<InsightResponse> = {};
+  mainKeys.forEach((key) => {
+    result[key as keyof InsightResponse] = {
+      evidence: "",
+      recommendation: {
+        root_cause: "",
+        action_steps: [],
+        business_impact: "",
+        success_metrics: "",
+        timeline: "",
+      },
+    };
+  });
   Object.entries(llmInsights).forEach(([key, value]) => {
     result[key as keyof InsightResponse] = {
       evidence: value?.evidence,
       recommendation: {
         root_cause: value?.recommendation?.root_cause || "",
-        action_steps: value?.recommendation?.action_steps || [""],
+        action_steps: value?.recommendation?.action_steps || [],
         business_impact: value?.recommendation?.business_impact || "",
         success_metrics: value?.recommendation?.success_metrics || "",
         timeline: value?.recommendation?.timeline || "",
